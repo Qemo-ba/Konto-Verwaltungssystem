ba6b1383cd862fa84f23b12ffc097ff37e8c8c35
Qemal Badalli <badalliqemal@gmail.com>
2026-06-25 21:59:15 +0200

    random

--- KVS_API/obj/Debug/net10.0/KVS_API.AssemblyInfo.cs
+++ KVS_API/obj/Debug/net10.0/KVS_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KVS_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c0d4cafe5bf4f708c781806e7d9981bed3a6ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e097f9d6889a72b77977454e4f5dbf64f56dbc86")]
 [assembly: System.Reflection.AssemblyProductAttribute("KVS_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KVS_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
